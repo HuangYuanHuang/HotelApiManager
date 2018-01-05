be19f0d63e42ea8f33fe13b7f5e461dd1874cdf4
HuangYuanHuang <834773759@qq.com>
2018-01-05 17:54:26 +0800

    今日订单接口

---
+++ HostelManager/Controllers/Api/HotelTodayGrabController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HostelManager.Models;
+
+namespace HostelManager.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/HotelTodayGrab")]
+    public class HotelTodayGrabController : BaseApiController
+    {
+
+        /// <summary>
+        /// 获取酒店今日（抢单类型）人员列表
+        /// </summary>
+        /// <param name="id">酒店GUID</param>
+        /// <returns></returns>
+        // GET: api/HotelTodayGrab/5
+        [HttpGet("{id}")]
+        public IEnumerable<HotelGrabGroupModel> Get(string id)
+        {
+
+            var todayOrders = hostelContext.HotelOrders.Where(d => d.OrderType == 1 && d.Hotel.GUID == id && d.Start == DateTime.Now.Date).Select(d => new HotelTodayGrabModel
+            {
+                DepartMentName = d.Department.DepartmentName,
+                OrderId = d.Id,
+                OrderGUID = d.GUID,
+                RoomNum = d.Num,
+                Start = d.Start.ToString("yyyy-MM-dd")
+
+            }).ToList();
+            foreach (var item in todayOrders)
+            {
+                item.POrders = hostelContext.PersonOrders.Where(d => d.OrderId == item.OrderId).Select(d => new PersonOrderDetail()
+                {
+                    Person = d.Person,
+                   
+                    ApplyNum = d.ApplyNum ?? 0,
+                    GUID = d.GUID,
+                    POrderId = d.Id,
+                }).ToList();
+
+            }
+            return todayOrders.GroupBy(d => d.DepartMentName, (key, values) => new HotelGrabGroupModel { DepartMentName = key, Orders = values });
+
+        }
+
+    }
+}
---
+++ HostelManager/Models/HotelTodayGrabModel.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostelManager.Models
+{
+    public class HotelGrabGroupModel
+    {
+        /// <summary>
+        /// 部门
+        /// </summary>
+        public string DepartMentName { get; set; }
+
+        /// <summary>
+        /// 订单列表
+        /// </summary>
+        public IEnumerable<HotelTodayGrabModel> Orders { get; set; }
+    }
+    public class HotelTodayGrabModel
+    {
+        /// <summary>
+        /// 部门
+        /// </summary>
+        [JsonIgnore]
+        public string DepartMentName { get; set; }
+
+
+        public int OrderId { get; set; }
+
+        public string OrderGUID { get; set; }
+
+        /// <summary>
+        /// 订单房间数
+        /// </summary>
+        public int RoomNum { get; set; }
+
+        public string Start { get; set; }
+
+        /// <summary>
+        /// 人员详情
+        /// </summary>
+        public IEnumerable<PersonOrderDetail> POrders { get; set; }
+    }
+}
